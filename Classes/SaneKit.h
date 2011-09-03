1b507105806b5696d8d249a6c860e3f0662dab5f
mkroehnert <mkroehnert@users.sourceforge.net>
2011-09-03 17:07:35 +0200

    add new class SaneKit

---
+++ Classes/SaneKit.h
@@ -0,0 +1,16 @@
+//
+//  SaneKit.h
+//  SaneKit
+//
+//  Created by MK on 03.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SaneKit : NSObject {
+
+}
+
+@end
---
+++ Classes/SaneKit.m
@@ -0,0 +1,14 @@
+//
+//  SaneKit.m
+//  SaneKit
+//
+//  Created by MK on 03.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SaneKit.h"
+
+@implementation SaneKit
+
+
+@end
