ea16df69ec9d047f6f638bb217e96aa9892f6f7f
mkroehnert <mkroehnert@users.sourceforge.net>
2011-09-03 16:28:19 +0200

    initial import of empty SaneKit project files

---
+++ Classes/SKScanManager.h
@@ -0,0 +1,16 @@
+//
+//  SKScanManager.h
+//  SaneKit
+//
+//  Created by Manfred Kroehnert on 03.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SKScanManager : NSObject {
+
+}
+
+@end
---
+++ Classes/SKScanManager.m
@@ -0,0 +1,14 @@
+//
+//  SKScanManager.m
+//  SaneKit
+//
+//  Created by MK on 03.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SKScanManager.h"
+
+
+@implementation SKScanManager
+
+@end
