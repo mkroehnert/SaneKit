d0ef1a4a2803a4adbdbcfeabc5c72c771e66d8dd
mkroehnert <mkroehnert@users.sourceforge.net>
2011-09-03 19:15:08 +0200

    scantool: print the options for the last of the found scan devices

--- Tools/scantool.m
+++ Tools/scantool.m
@@ -21,7 +21,11 @@ int main(int argc, char* argv[])
     if (0 < [deviceList count])
     {
         SKScanDevice* device = [deviceList lastObject];
-        BOOL openStatus = [device open];
+        BOOL success = [device open];
+        if (success)
+        {
+             [device printOptions];
+        }
         
         [device close];
     }
